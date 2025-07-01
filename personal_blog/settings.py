173d9b6f701621b43c4a515c8508a292096be5d5
sebe324 <sebeq05@gmail.com>
2025-07-01 19:42:45 +0200

    fix settings

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()
-import personal_blog.env as env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
