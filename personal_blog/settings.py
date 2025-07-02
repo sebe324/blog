ef3d609eea5e1a78e56e2f96bd8b842faac15651
sebe324 <sebeq05@gmail.com>
2025-07-02 20:14:02 +0200

    wsgi .env fix

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY","")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG","False")
 URL_TEST = os.getenv("URL","")
 
 ALLOWED_HOSTS = ['127.0.0.1',os.getenv("IP",""),os.getenv("URL",""), "www."+os.getenv("URL","")]
